90e18bb7c613da0a83399330f50f09a925113468
Franci Nakamura <francinakamura@FranciNrasiMac3.attlocal.net>
2020-04-16 12:55:57 -0700

    Added radio buttons, select boxes & Submit button

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ class App extends React.Component {
       this.state = {
         firstName: " ",
         lastName: " ",
-        isFriendly: false
-      
+        isFriendly: false,
+        gender: " ", 
+        favColor: "blue"
       }
   this.handleChange = this.handleChange.bind(this)    
       
@@ -20,7 +21,7 @@ class App extends React.Component {
   render () {
     return (
 
-      <form>
+      <form onSubmit = {this.handleSubmit}>
         <input type = "text" 
                value = {this.state.value}
                name ="firstName" 
@@ -50,7 +51,58 @@ class App extends React.Component {
               <br/>
               
         </label>
+
+        <label>
+            <br/>
+            
+              <input
+                  type = "radio" 
+                  name = "gender"
+                  value = "female"
+                  checked = {this.state.gender === "female"}
+                  onChange = {this.handleChange}
+              /> 
+              Female
+              <br/>
+              
+        </label>
+
+        <label>
+            <br/>
+            
+              <input
+                  type = "radio"
+                  name = "gender"
+                  value = "male"
+                  checked = {this.state.gender === "male" }
+                  onChange = {this.handleChange}
+              /> 
+              Male
+              <br/>
+              
+        </label>
+
+        <label>Favorite Color:</label>
+        <select
+              value = {this.state.favColor}
+              onChange = {this.handleChange}
+              name = "favColor"
+            >
+              <option value = "blue" > Blue</option>
+              <option value = "green" > Green</option>
+              <option value = "yellow" > Yellow</option>
+              <option value = "orange" > Orange</option>
+              <option value = "purple" > Purple</option>
+              <option value = "red" > red</option>
+
+        </select>
+
+
           <h1>{this.state.firstName} {this.state.lastName} </h1>
+          <h2> You are a {this.state.gender}</h2>
+          <h3> Your favorite color is {this.state.favColor}</h3>
+
+          <button>Submit</button>
       </form>
       
 
