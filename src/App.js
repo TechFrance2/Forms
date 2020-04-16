ce5a07d41b11b99228e5106062df4bbe189d245f
Franci Nakamura <francinakamura@FranciNrasiMac3.attlocal.net>
2020-04-15 23:28:39 -0700

    Add checkbox and text area

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor () {
+    super ()
+      this.state = {
+        firstName: " ",
+        lastName: " ",
+        isFriendly: false
+      
+      }
+  this.handleChange = this.handleChange.bind(this)    
+      
+  } 
+    handleChange (event) {
+      const {name, value, type, checked} = event.target
+      type === "checkbox" ? this.setState ({[name]: checked}) : this.setState ({[name]: value })
+  }
+  render () {
+    return (
+
+      <form>
+        <input type = "text" 
+               value = {this.state.value}
+               name ="firstName" 
+               placeholder = "First Name" 
+               onChange = {this.handleChange}
+         />
+           <br/>    
+        <input type = "text" 
+               value = {this.state.value}
+               name = "lastName"
+               placeholder = "Last Name" 
+               onChange = {this.handleChange}
+          />
+          <br/>
+          <textarea value = {"Please check box below"} 
+                    onChange = {this.handleChange}/>
+          <label>
+            <br/>
+            
+              <input
+                  type = "checkbox" 
+                  name = {"isFriendly"}
+                  checked = {this.state.isFriendly }
+                  onChange = {this.handleChange}
+              /> 
+              Is friendly?
+              <br/>
+              
+        </label>
+          <h1>{this.state.firstName} {this.state.lastName} </h1>
+      </form>
+      
+
+    )
+  }
 }
 
-export default App;
+export default App
+
